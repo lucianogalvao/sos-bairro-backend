9f6bc5ab434e458bd10ce074ac381b143e2e8a80
Luciano <luciano.g.galvaao@gmail.com>
2025-12-09 20:03:58 -0300

    feat: implement authentication and user management modules with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { OccurrenceCategoriesModule } from './occurrence-categories/occurrence-categories.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import { OccurrenceCategoriesModule } from './occurrence-categories/occurrence-c
     }),
     PrismaModule,
     OccurrenceCategoriesModule,
+    UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
