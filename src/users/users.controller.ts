e8266d97b317bb9e4cdd6d0df124df0701b38858
Luciano <luciano.g.galvaao@gmail.com>
2025-12-15 21:46:04 -0300

    feat: enhance UsersController and UsersService with roles-based access and improved user retrieval

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,30 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Role } from '@prisma/client';
 import { Request } from 'express';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
 import { AuthenticatedUser } from 'src/types';
+import { UsersService } from './users.service';
 
 interface AuthenticatedRequest extends Request {
   user: AuthenticatedUser;
 }
+
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @UseGuards(JwtAuthGuard)
   @Get('my-profile')
   getMe(@Req() req: AuthenticatedRequest) {
     return req.user;
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Get()
+  async findAll() {
+    return this.usersService.findAll();
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -24,9 +24,19 @@ export class UsersService {
     });
   }
 
-  findAll() {
+  async findAll() {
     return this.prisma.user.findMany({
-      orderBy: { id: 'asc' },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+      orderBy: {
+        createdAt: 'asc',
+      },
     });
   }
 }
